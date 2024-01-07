5576ea8c79321bbd2332c16f5ba9795622297d69
maxi-rzp <maximilian.rosenzopf@gmail.com>
2024-01-07 18:42:02 +0100

    backend adminfile

--- Diplomarbeit-EVG/ClientApp/src/app/app.component.ts
+++ Diplomarbeit-EVG/ClientApp/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
---
+++ Diplomarbeit-EVG/Controllers/GlobalVariables.cs
@@ -0,0 +1,7 @@
+﻿namespace Diplomarbeit_EVG.ClientApp.src.app
+{
+    public static class GlobalVariables
+    {
+        public static byte[] GlobalVar { get; set; }
+    }
+}
